d0fb55a9194264452cdf710203c6cfb6de5714d8
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-27 19:16:38 -0400

    Add filetrees for SearchBar, SearchResults && PlayList components && css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,10 +7,10 @@ function App() {
         Ju<span className="highlight">keb</span>oxd
       </h1>
       <div className="App">
-        {/* <!-- Add a SearchBar component --> */}
+        {/* <!-- TODO: Add a SearchBar component --> */}
         <div className="App-playlist">
-          {/* <!-- Add a SearchResults component -->
-      <!-- Add a Playlist component --> */}
+          {/* <!-- TODO: Add a SearchResults component -->
+            <!-- TODO: Add a Playlist component --> */}
         </div>
       </div>
     </div>
