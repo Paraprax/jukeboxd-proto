aee0775f7fe7ad68b979d42ab97cb81d3de32f70
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-27 21:18:24 -0400

    Wrote renderAction method w/ conditional for rendering Add or Remove button

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 export class Track extends React.Component {
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button className="Track-action">-</button>; //removal-button
+    } else {
+      return <button className="Track-action">+</button>; //adder-button
+    }
+  }
   render() {
     return (
-      <div class="Track">
-        <div class="Track-information">
+      <div className="Track">
+        <div className="Track-information">
           {/* <h3><!-- TODO: render track name --></h3> */}
           {/* <p><!-- TODO: render artist name --> | <!-- TODO: render album name --></p> */}
         </div>
-        {/* <button class="Track-action"><!-- TODO: render '+' and '-' buttons --></button> */}
+        {this.renderAction}
       </div>
     );
   }
