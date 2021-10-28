c4bf4f54127aab19f57bcaef4286ac0edcb75452
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-27 21:12:23 -0400

    Created Track directory, component && css

---
+++ src/components/Track/Track.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class Track extends React.Component {
+  render() {
+    return (
+      <div class="Track">
+        <div class="Track-information">
+          {/* <h3><!-- TODO: render track name --></h3> */}
+          {/* <p><!-- TODO: render artist name --> | <!-- TODO: render album name --></p> */}
+        </div>
+        {/* <button class="Track-action"><!-- TODO: render '+' and '-' buttons --></button> */}
+      </div>
+    );
+  }
+}
