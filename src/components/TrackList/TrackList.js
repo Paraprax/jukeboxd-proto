4cbbd8325904ecc7cb5bbaf0244fa7aa2ad4f9e9
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-10-27 21:05:54 -0400

    Created TrackList component

---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class TrackList extends React.Component {
+  render() {
+    return <div className="TrackList">{/* <!-- TODO: Render a set of track components using array mapping  --> */}</div>;
+  }
+}
