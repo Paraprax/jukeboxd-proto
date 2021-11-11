fbb142181d43442de128cf581258229309285509
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-11-10 21:57:39 -0500

    Refactor App component/syntax

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,21 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <h1>
-        Jukeboxd
-      </h1>
-      <div className="App">
-        {/* <!-- TODO: Add a SearchBar component --> */}
-        <div className="App-playlist">
-          {/* <!-- TODO: Add a SearchResults component -->
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Jukeboxd</h1>
+        <div className="App">
+          {/* <!-- TODO: Add a SearchBar component --> */}
+          <div className="App-playlist">
+            {/* <!-- TODO: Add a SearchResults component -->
             <!-- TODO: Add a Playlist component --> */}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
