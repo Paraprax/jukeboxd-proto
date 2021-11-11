d0d4d61f73e67471abb21292faa9cb8d5be5688a
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-11-11 17:04:14 -0500

    Pass each track prop to Track component using array mapping

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -6,11 +6,14 @@ export class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {/* <!-- TODO: Render a set of track components using array mapping  --> */}
-        <p>Orchestral Manoeuvers In The Dark - Enola Gay</p>
-        <p>New Order - Crystal</p>
-        <p>Vangelis - Blade Runner Blues</p>
+        {this.props.tracks.map((track) => (
+          <Track track={track} />
+        ))}
       </div>
     );
   }
 }
+
+/* <p>Orchestral Manoeuvers In The Dark - Enola Gay</p>
+<p>New Order - Crystal</p>
+<p>Vangelis - Blade Runner Blues</p> */
