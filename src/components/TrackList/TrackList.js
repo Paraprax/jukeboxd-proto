33faae43c420c9cc916376c9ab34348dfce3d0a2
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-11-17 18:32:43 -0500

    Refactor map statement in TrackList.js

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -6,9 +6,9 @@ export class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {this.props.tracks.map((track) => (
-          <Track track={track} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />
-        ))}
+        {this.props.tracks.map((track) => {
+          return <Track track={track} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />;
+        })}
       </div>
     );
   }
